70af3a843b0f667e2a579d417f107c7ecfd96542
N1kiT0S <canngabach5869@gmail.com>
2023-05-08 16:53:14 +0300

    start

---
+++ src/main/java/com/example/demo/MiskaRisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiskaRisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiskaRisaApplication.class, args);
+	}
+
+}
